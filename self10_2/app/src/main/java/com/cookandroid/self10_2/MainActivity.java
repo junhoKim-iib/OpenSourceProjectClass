e5abf28e56814d57aad6e376c23bfdaeb53894ee
deeptoo2 <deeptoo2@naver.com>
2023-04-16 01:47:07 +0900

    self10_2

---
+++ self10_2/app/src/main/java/com/cookandroid/self10_2/MainActivity.java
@@ -0,0 +1,63 @@
+package com.cookandroid.self10_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("명화 선호도 투표 직접해보기");
+
+        final int voteCount[] = new int[9];
+        for (int i = 0; i < 9; i++)
+            voteCount[i] = 0;
+
+        ImageView image[] = new ImageView[9];
+
+        Integer imageId[] = { R.id.iv1, R.id.iv2, R.id.iv3, R.id.iv4, R.id.iv5,
+                R.id.iv6, R.id.iv7, R.id.iv8, R.id.iv9 };
+
+        final String imgName[] = { "모나리자", "최후의 만찬", "리타의 성모",
+                "Decemviri Altarpiece", "비앙카 마리아 스포르차", "The Madonna and Child with Angels", "절규", "진주 귀고리를 한 소녀",
+                "비너스의 탄생" };
+
+        for (int i = 0; i < imageId.length; i++) {
+            final int index;
+            index = i;
+            image[index] = (ImageView) findViewById(imageId[index]);
+            image[index].setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+
+
+                    voteCount[index]++;
+                    Toast.makeText(getApplicationContext(),
+                            imgName[index] + ": 총 " + voteCount[index] + " 표",
+                            Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+
+        Button btnFinish = (Button) findViewById(R.id.btnResult);
+        btnFinish.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),
+                        ResultActivity.class);
+                intent.putExtra("VoteCount", voteCount);
+                intent.putExtra("ImageName", imgName);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+}
---
+++ self10_2/app/src/main/java/com/cookandroid/self10_2/ResultActivity.java
@@ -0,0 +1,71 @@
+package com.cookandroid.self10_2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.result);
+        setTitle("투표 결과");
+
+
+        Intent intent = getIntent();
+        int[] voteResult = intent.getIntArrayExtra("VoteCount");
+        String[] imageName = intent.getStringArrayExtra("ImageName");
+
+        Integer imageFileId[] = { R.drawable.draw1, R.drawable.draw2,
+                R.drawable.draw3, R.drawable.draw4, R.drawable.draw5,
+                R.drawable.draw6, R.drawable.draw7, R.drawable.draw8,
+                R.drawable.draw9 };
+
+
+        TextView tvTop = (TextView) findViewById(R.id.tvTop);
+        ImageView ivTop = (ImageView) findViewById(R.id.ivTop);
+        int maxEntry = 0;
+        for (int i = 1; i < voteResult.length; i++) {
+            if (voteResult[maxEntry] < voteResult[i])
+                maxEntry = i;
+        }
+        tvTop.setText(imageName[maxEntry]);
+        ivTop.setImageResource(imageFileId[maxEntry]);
+
+        TextView tv[] = new TextView[imageName.length];
+        RatingBar rbar[] = new RatingBar[imageName.length];
+
+        Integer tvID[] = { R.id.tv1, R.id.tv2, R.id.tv3, R.id.tv4, R.id.tv5,
+                R.id.tv6, R.id.tv7, R.id.tv8, R.id.tv9 };
+        Integer rbarID[] = { R.id.rbar1, R.id.rbar2, R.id.rbar3, R.id.rbar4,
+                R.id.rbar5, R.id.rbar6, R.id.rbar7, R.id.rbar8, R.id.rbar9 };
+        
+        for (int i = 0; i < voteResult.length; i++) {
+            tv[i] = (TextView) findViewById(tvID[i]);
+            rbar[i] = (RatingBar) findViewById(rbarID[i]);
+        }
+
+        for (int i = 0; i < voteResult.length; i++) {
+            tv[i].setText(imageName[i]);
+            rbar[i].setRating((float) voteResult[i]);
+        }
+
+        Button btnReturn = (Button) findViewById(R.id.btnReturn);
+        btnReturn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                finish();
+
+            }
+        });
+
+    }
+
+}
