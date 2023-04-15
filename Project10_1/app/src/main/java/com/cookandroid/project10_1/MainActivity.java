f655f2bffe578794e1b177e598d79ec9a9e3069a
deeptoo2 <deeptoo2@naver.com>
2023-04-15 22:23:42 +0900

    10_1

---
+++ Project10_1/app/src/main/java/com/cookandroid/project10_1/MainActivity.java
@@ -0,0 +1,25 @@
+package com.cookandroid.project10_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnNewActivity = (Button) findViewById(R.id.btnNewActivity);
+        btnNewActivity.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
